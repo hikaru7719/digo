70d3eaf443f62c7c0853cf6c0f9519aca4bc78ab
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2019-04-08 20:54:28 +0900

    packetの内容を理解した

--- main.go
+++ main.go
@@ -8,17 +8,37 @@ import (
 )
 
 func main() {
-	conn, err := net.Dial("udp", "192.168.2.1:53")
+	conn, err := net.Dial("udp", "172.20.10.1:53")
 	defer conn.Close()
 	if err != nil {
-		fmt.Println("a")
 		panic(err)
 	}
 
 	_, err = conn.Write([]byte{0x00, 0x00, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x77, 0x77, 0x77, 0x04, 0x6a, 0x70, 0x72, 0x73, 0x02, 0x63, 0x6f, 0x02, 0x6a, 0x70, 0x00, 0x00, 0x01, 0x00, 0x01})
 	if err != nil {
-		fmt.Println("b")
 		fmt.Println(err)
 	}
 	io.Copy(os.Stdout, conn)
+
+	// Headerは24byte
+	headBuffer := make([]byte, 24)
+	_, err = conn.Read(headBuffer)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("head:%X", headBuffer)
+	// Questionは40byte
+	questionBuffer := make([]byte, 40)
+	_, err = conn.Read(questionBuffer)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// Answerは32byte
+	answerBuffer := make([]byte, 32)
+	_, err = conn.Read(answerBuffer)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%X", answerBuffer)
+
 }
