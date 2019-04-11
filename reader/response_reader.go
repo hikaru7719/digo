f030670112516d30a5af27fdfe205d0fb8801ac2
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2019-04-11 21:08:24 +0900

    testを書いた

--- reader/response_reader.go
+++ reader/response_reader.go
@@ -1,6 +1,7 @@
 package reader
 
 import (
+	"encoding/binary"
 	"errors"
 	"fmt"
 	"reflect"
@@ -10,32 +11,50 @@ import (
 const headSize = 12
 
 type ResponseReader struct {
-	query     []byte
-	response  []byte
-	header    []byte
-	aRecord   int
-	nsRecord  int
-	addRecord int
+	query       []byte
+	queryLength int
+	response    []byte
+	header      []byte
+	aRecord     uint16
+	nsRecord    uint16
+	addRecord   uint16
 }
 
-func NewResponseReader(query, packet []byte) *ResponseReader {
-	return &ResponseReader{query: query, response: packet}
+func NewResponseReader(query, response []byte) *ResponseReader {
+	return &ResponseReader{query: query, queryLength: len(query), response: response}
 }
 
 func (r *ResponseReader) GetHeader() {
 	r.header = r.response[:headSize]
 }
 func (r *ResponseReader) ReadHeader() error {
+	r.GetHeader()
 	var current int
-	_ := r.header[:2]
+	id := r.header[:2]
+	fmt.Println(id)
 	current += 2
 	errFlag := r.header[current : current+2]
-	if reflect.DeepEqual(errFlag, []byte{0x81, 0x80}) {
+	if !reflect.DeepEqual(errFlag, []byte{0x81, 0x80}) {
 		fmt.Println("error flag")
 		return errors.New("invalid flag")
 	}
 	current += 2
+	qCount := r.header[current : current+2]
+	fmt.Println(binary.BigEndian.Uint16(qCount))
+	current += 2
 	aCount := r.header[current : current+2]
+	fmt.Println(binary.BigEndian.Uint16(aCount))
+	r.aRecord = binary.BigEndian.Uint16(aCount)
+	current += 2
 
+	nsCount := r.header[current : current+2]
+	fmt.Println(binary.BigEndian.Uint16(nsCount))
+	r.nsRecord = binary.BigEndian.Uint16(nsCount)
+	current += 2
+
+	addCount := r.header[current : current+2]
+	fmt.Println(binary.BigEndian.Uint16(addCount))
+	r.addRecord = binary.BigEndian.Uint16(addCount)
+	current += 2
 	return nil
 }
---
+++ reader/response_reader_test.go
@@ -0,0 +1,45 @@
+package reader
+
+import "testing"
+
+func TestResponseReader_ReadHeader(t *testing.T) {
+	cases := map[string]struct {
+		reader          *ResponseReader
+		expectARecord   uint16
+		expectNSRecord  uint16
+		expectAddRecord uint16
+	}{
+		"success": {
+			reader:          testNewResponseReader(t),
+			expectARecord:   1,
+			expectNSRecord:  0,
+			expectAddRecord: 0,
+		},
+	}
+
+	for n, tc := range cases {
+		t.Run(n, func(t *testing.T) {
+			tc.reader.ReadHeader()
+			if tc.reader.aRecord != tc.expectARecord {
+				t.Errorf("want %d, but actual %d\n", tc.expectARecord, tc.reader.aRecord)
+			}
+
+			if tc.reader.nsRecord != tc.expectNSRecord {
+				t.Errorf("want %d, but actual %d\n", tc.expectNSRecord, tc.reader.nsRecord)
+			}
+
+			if tc.reader.addRecord != tc.expectAddRecord {
+				t.Errorf("want %d, but actual %d\n", tc.expectAddRecord, tc.reader.addRecord)
+			}
+		})
+	}
+}
+
+func testNewResponseReader(t *testing.T) *ResponseReader {
+	testResponse := []byte{
+		0x00, 0x00, 0x81, 0x80, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x03, 0x77, 0x77, 0x77,
+		0x04, 0x6a, 0x70, 0x72, 0x73, 0x02, 0x63, 0x6f, 0x02, 0x6a, 0x70, 0x00, 0x00, 0x01, 0x00, 0x01,
+		0xc0, 0x0c, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x01, 0x2c, 0x00, 0x04, 0x75, 0x68, 0x85, 0xa5,
+	}
+	return NewResponseReader(nil, testResponse)
+}
